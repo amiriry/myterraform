9b3c11542edf2a0600d3976269c954a7398a5489
amirshkedy <amirshkedy@shkedy.co.il>
2020-11-12 13:51:28 +0200

    tf file for creating instance in amazon

---
+++ aws_instance.1.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+	profile = "default"
+	region = var.region
+}
+
+# Create amzn2 instance with sg security group, that should already exist and keypair named keypair1111
+# The instance name wil be WebServer01
+resource "aws_instance" "webserver01" {
+	ami = "ami-0947d2ba12ee1ff75"
+	instance_type = "t2.micro"
+	security_groups = ["sg"]
+	user_data = <<-EOF
+					#!/bin/bash
+					yum update -y
+					yum install -y httpd
+					EOF
+	key_name = "keypair1111"
+
+	tags = {
+		Name = "WebServer01"
+	}
+}
