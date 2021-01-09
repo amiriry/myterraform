9e97faed984bce027235bc098dd8af567591e9c0
amirshkedy <amirshkedy@shkedy.co.il>
2021-01-09 18:47:57 +0200

    one instance manifest for gcloud

---
+++ google/one-instance/instance.tf
@@ -0,0 +1,18 @@
+resource "google_compute_instance" "default" {
+	name = "fuckthis"
+	machine_type = "f1-micro"
+	zone = "us-central1-a"
+	boot_disk {
+		initialize_params {
+			image = "debian-cloud/debian-9"
+		}
+	}
+
+	network_interface {
+		network = "default"
+	}
+
+	metadata = {
+		foo = "bar"
+	}
+}
---
+++ google/one-instance/provider.tf
@@ -0,0 +1,5 @@
+provider "google" {
+	credentials = file("playground-s-11-2c60f632-2c98c02c24b7.json")
+	project = "playground-s-11-2c60f632"
+	region = "us-west1"
+}
